d2181f8f9017eb2936269b966bfe3d87e1430c5f
adamchen3 <weiguan.chen@outlook.com>
2021-11-01 21:22:29 +0800

    Revert "try to optimize alloc time"
    This reverts commit 425d260dfe13e2197ee0bc1a0d6d501f0d913906.

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -141,7 +141,7 @@ kalloc(void)
 void *
 stealmemory(int cpuid)
 {
-  int id = cpuid + NCPU/2; //从远一点开始steal
+  int id = cpuid + 1;
   struct run *r = 0;
   for (int i = 0; i < NCPU; i++) {
     if (id >= NCPU) {
@@ -149,7 +149,7 @@ stealmemory(int cpuid)
     }
 
     if (cpuid == id) {
-      continue;
+      break;
     }
 
     struct cpu *cpu = &cpus[id];
